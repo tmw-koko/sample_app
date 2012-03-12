4f98c3b62aee9a99f68c98417cffd6207e1a6bb7
Tim Woodbury <tim.woodbury@kokofitclub.com>
2012-03-12 13:28:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a3c220b05bbab8a0b0c47c1d5b30a84f12d8d52ce8139121aa0289aa87a61e897366352e1e232c7bbde6496e74111f1abc5ed38a49da59a9d713d62aa1fb45df'
