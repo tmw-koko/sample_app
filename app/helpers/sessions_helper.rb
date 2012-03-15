22276ea43f2f1984bd9378d43c42a9d69328f33a
Tim Woodbury <tim.woodbury@kokofitclub.com>
2012-03-15 13:58:59 -0400

    Finish Ch. 8 exercises

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    session[:remember_token] = user.remember_token
     current_user = user
   end
   
@@ -11,7 +11,7 @@ module SessionsHelper
   
   def sign_out
     current_user = nil
-    cookies.delete :remember_token
+    session[:remember_token] = nil
   end
   
   def current_user=(user)
@@ -24,7 +24,7 @@ module SessionsHelper
   
   private
     def user_from_remember_token
-      remember_token = cookies[:remember_token]
+      remember_token = session[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
   
